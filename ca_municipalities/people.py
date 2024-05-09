b6f7ca09f095f4ca70e385179e212699e813fbc9
Rafe Murray <rafe.murray@jmaconsulting.biz>
2024-05-09 10:26:49 -0400

    [WIP] Update municipalities scraper

--- ca_municipalities/people.py
+++ ca_municipalities/people.py
@@ -11,6 +11,16 @@ class CanadaMunicipalitiesPersonScraper(CSVScraper):
     csv_url = "https://docs.google.com/spreadsheets/d/e/2PACX-1vRrGXQy8qk16OhuTjlccoGB4jL5e8X1CEqRbg896ufLdh67DQk9nuGm-oufIT0HRMPEnwePw2HDx1Vj/pub?gid=0&single=true&output=csv"
     encoding = "utf-8"
 
+    corrections = {
+        "organization":{
+            "Orillia": "Orillia, City of",
+            "Loon Lake": "Loon Lake No. 561",
+        },
+        "district":{
+            "Loon Lake": "Loon Lake No. 561"
+        }
+    }
+    
     """
     Returns whether the row should be imported.
     """
